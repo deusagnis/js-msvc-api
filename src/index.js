c6956feecd15651561c37f14efbdc88f29d23040
Deusagnis <deusagnis@gmail.com>
2021-09-27 15:55:56 +0300

    Format code. Update git in package.json.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-module.exports = class MsvcApi
-{
+module.exports = class MsvcApi {
     proxy
 
     apiUrl
@@ -26,34 +25,32 @@ module.exports = class MsvcApi
 
     response
 
-    errorsHandler = (error) => console.log(error.name + ': ' + error.message)
-
-    constructor (api,msvc)
-    {
+    constructor(api, msvc) {
         this.apiUrl = api
         this.msvcName = msvc
 
         return this.asProxy()
     }
 
-    asProxy(){
+    errorsHandler = (error) => console.log(error.name + ': ' + error.message)
+
+    asProxy() {
         this.proxy = this.createProxy()
         return this.proxy
     }
 
-    createProxy(){
+    createProxy() {
         let handler = {
-            get: function (parent, prop)
-            {
+            get: function (parent, prop) {
                 if (typeof parent[prop] !== 'undefined')
                     return parent[prop]
 
-                if(parent.isObjectCall()){
+                if (parent.isObjectCall()) {
                     parent.setObjectName(prop)
                     parent.resetActionName()
 
                     return parent.proxy;
-                }else if (parent.isActionCall()){
+                } else if (parent.isActionCall()) {
                     parent.setActionName(prop)
 
                     return parent.setDataToSend
@@ -64,92 +61,92 @@ module.exports = class MsvcApi
         return new Proxy(this, handler)
     }
 
-    isObjectCall(){
-        if(this.objectName === null || this.actionName !== null ){
+    isObjectCall() {
+        if (this.objectName === null || this.actionName !== null) {
             return true
         }
 
         return false
     }
 
-    isActionCall(){
-        if(this.objectName !== null && this.actionName === null){
+    isActionCall() {
+        if (this.objectName !== null && this.actionName === null) {
             return true
         }
 
         return false
     }
 
-    setObjectName(name){
+    setObjectName(name) {
         this.objectName = name
     }
 
-    resetActionName(){
+    resetActionName() {
         this.actionName = null
     }
 
-    setActionName(name){
+    setActionName(name) {
         this.actionName = name
     }
 
-    setDataToSend(data= null){
+    setDataToSend(data = null) {
         this.dataToSend = data
 
         return this.proxy
     }
 
 
-    get(){
+    get() {
         this.fetchSettings.method = this.getMethod
         this.removeRequestBody()
 
         return this.proxy
     }
 
-    removeRequestBody(){
-        if(this.fetchSettings['body'] !== undefined){
+    removeRequestBody() {
+        if (this.fetchSettings['body'] !== undefined) {
             delete this.fetchSettings['body']
         }
     }
 
-    post(){
+    post() {
         this.fetchSettings.method = this.postMethod
 
         return this.proxy
     }
 
-    form(){
+    form() {
         this.post()
         this.resetContentType()
 
         return this.proxy
     }
 
-    json(){
+    json() {
         this.post()
         this.contentJson()
 
         return this.proxy
     }
 
-    async send(){
+    async send() {
         this.provideFileExistence()
 
         const target = this.createRequestTarget()
         this.provideBody()
 
-        try{
-            this.response = await fetch(target,this.fetchSettings)
+        try {
+            this.response = await fetch(target, this.fetchSettings)
 
             return await this.handleResponse()
-        }catch (e){
+        } catch (e) {
             await this.errorsHandler(e)
 
             return false
         }
     }
 
-    async handleResponse(){
+    async handleResponse() {
         this.handleServerErrors()
 
         const decodedResponse = await this.response.json()
@@ -159,24 +156,24 @@ module.exports = class MsvcApi
         return decodedResponse
     }
 
-    handleServerErrors(){
-        if(this.response.ok === false){
+    handleServerErrors() {
+        if (this.response.ok === false) {
             throw new Error('Server error with code ' + this.response.status)
         }
     }
 
-    handleResponseErrors(decodedResponse){
-        if(decodedResponse['errors'] === undefined) return
+    handleResponseErrors(decodedResponse) {
+        if (decodedResponse['errors'] === undefined) return
 
         const responseError = decodedResponse['errors'].shift()
 
         throw new Error(responseError['mess'])
     }
 
-    createRequestTarget(){
+    createRequestTarget() {
         let target = this.createRequestUrl()
 
-        if(this.fetchSettings.method === this.getMethod){
+        if (this.fetchSettings.method === this.getMethod) {
             target += '?'
             target += this.createDataQuery()
         }
@@ -184,16 +181,16 @@ module.exports = class MsvcApi
         return target
     }
 
-    createDataQuery(){
+    createDataQuery() {
         let params = new URLSearchParams(this.dataToSend)
 
         return params.toString()
     }
 
-    createRequestUrl(){
-        let parts = [this.apiUrl,this.msvcName]
+    createRequestUrl() {
+        let parts = [this.apiUrl, this.msvcName]
 
-        if(this.objectName !== this.msvcSelfObjectKey){
+        if (this.objectName !== this.msvcSelfObjectKey) {
             parts.push(this.objectName)
         }
 
@@ -202,17 +199,17 @@ module.exports = class MsvcApi
         return parts.join('/')
     }
 
-    provideFileExistence(){
-        if(this.dataToSendWithoutFiles()) return
+    provideFileExistence() {
+        if (this.dataToSendWithoutFiles()) return
 
         this.form()
     }
 
-    dataToSendWithoutFiles(){
-        if(typeof this.dataToSend !== 'object') return true
+    dataToSendWithoutFiles() {
+        if (typeof this.dataToSend !== 'object') return true
 
-        for (let key in this.dataToSend){
-            if(this.dataToSend[key] instanceof File){
+        for (let key in this.dataToSend) {
+            if (this.dataToSend[key] instanceof File) {
                 return false
             }
         }
@@ -220,38 +217,38 @@ module.exports = class MsvcApi
         return true
     }
 
-    contentJson(){
+    contentJson() {
         this.fetchSettings.headers['Content-Type'] = this.jsonContentType
 
         return this.proxy
     }
 
-    resetContentType(){
+    resetContentType() {
         delete this.fetchSettings.headers['Content-Type']
     }
 
 
-    provideBody(){
-        if(this.fetchSettings.method !== this.postMethod) return
+    provideBody() {
+        if (this.fetchSettings.method !== this.postMethod) return
 
         const contentType = this.fetchSettings.headers['Content-Type']
 
-        if(contentType === this.jsonContentType){
+        if (contentType === this.jsonContentType) {
             this.fetchSettings.body = this.createJsonBody()
-        }else{
+        } else {
             this.fetchSettings.body = this.createFormBody()
         }
     }
 
-    createJsonBody(){
+    createJsonBody() {
         return JSON.stringify(this.dataToSend)
     }
 
-    createFormBody(){
+    createFormBody() {
         const formData = new FormData()
 
-        for( let key in this.dataToSend){
-            formData.append(key,this.dataToSend[key])
+        for (let key in this.dataToSend) {
+            formData.append(key, this.dataToSend[key])
         }
 
         return formData
