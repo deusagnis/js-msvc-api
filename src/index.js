ba57881653a8761e4c218a62e43445443e0bcf52
Deusagnis <deusagnis@gmail.com>
2022-07-07 00:17:32 +0300

    Add comments.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 class MsvcApi {
+    /**
+     * Proxy of object.
+     */
     proxy
 
+    /**
+     * Url for requests to API.
+     */
     apiUrl
+    /**
+     * Name of microservice.
+     */
     msvcName
 
+    /**
+     * Name of API entity.
+     */
     objectName = null
+    /**
+     * Name of API action.
+     */
     actionName = null
 
+    /**
+     * Params of request.
+     */
     params
 
+    /**
+     * Entity name if action belongs to microservice.
+     * @type {string}
+     */
     msvcSelfObjectKey = 'it'
 
+    /**
+     * Fetch method settings.
+     */
     fetchSettings = {
         method: 'GET',
         mode: 'cors',
@@ -19,15 +44,41 @@ class MsvcApi {
         headers: {},
     }
 
+    /**
+     * Request content type for JSON.
+     * @type {string}
+     */
     jsonContentType = 'application/json'
+    /**
+     * Method for GET requests.
+     * @type {string}
+     */
     getMethod = 'GET'
+    /**
+     * Method for POST requests.
+     * @type {string}
+     */
     postMethod = 'POST'
 
+    /**
+     * Received response.
+     */
     response
+    /**
+     * Decoded received response.
+     */
     decodedResponse
 
+    /**
+     * Callbacks executed before send request.
+     */
     preparingCallbacks = []
 
+    /**
+     * @param api {string}
+     * @param msvc {string}
+     * @returns {MsvcApi}
+     */
     constructor(api, msvc) {
         this.apiUrl = api
         this.msvcName = msvc
@@ -35,13 +86,25 @@ class MsvcApi {
         return this.asProxy()
     }
 
+    /**
+     * Method for handling API errors.
+     * @param error
+     */
     errorsHandler = (error) => console.log(error.name + ': ' + error.message)
 
+    /**
+     * Make Proxy handler for MsvcApi object.
+     * @returns {MsvcApi}
+     */
     asProxy() {
         this.proxy = this.createProxy()
         return this.proxy
     }
 
+    /**
+     * Add handlers.
+     * @returns {MsvcApi}
+     */
     createProxy() {
         let handler = {
             get: function (parent, prop) {
@@ -64,6 +127,10 @@ class MsvcApi {
         return new Proxy(this, handler)
     }
 
+    /**
+     * Check if call of object.
+     * @returns {boolean}
+     */
     isObjectCall() {
         if (this.objectName === null || this.actionName !== null) {
             return true
@@ -72,6 +139,10 @@ class MsvcApi {
         return false
     }
 
+    /**
+     * Check if call of action.
+     * @returns {boolean}
+     */
     isActionCall() {
         if (this.objectName !== null && this.actionName === null) {
             return true
@@ -80,25 +151,44 @@ class MsvcApi {
         return false
     }
 
+    /**
+     * Setter for API entity name.
+     * @param name
+     */
     setObjectName(name) {
         this.objectName = name
     }
 
+    /**
+     * Remove API action name.
+     */
     resetActionName() {
         this.actionName = null
     }
 
+    /**
+     * Setter for API action name.
+     * @param name
+     */
     setActionName(name) {
         this.actionName = name
     }
 
+    /**
+     * Set request params.
+     * @param data
+     * @returns {MsvcApi}
+     */
     setParams(data = null) {
         this.params = data
 
         return this.proxy
     }
 
-
+    /**
+     * Set request method as GET.
+     * @returns {MsvcApi}
+     */
     get() {
         this.fetchSettings.method = this.getMethod
         this.removeRequestBody()
@@ -106,18 +196,29 @@ class MsvcApi {
         return this.proxy
     }
 
+    /**
+     * Remove request body from fetch settings.
+     */
     removeRequestBody() {
         if (this.fetchSettings['body'] !== undefined) {
             delete this.fetchSettings['body']
         }
     }
 
+    /**
+     * Set request method as POST.
+     * @returns {MsvcApi}
+     */
     post() {
         this.fetchSettings.method = this.postMethod
 
         return this.proxy
     }
 
+    /**
+     * Set request type as FORM.
+     * @returns {MsvcApi}
+     */
     form() {
         this.post()
         this.resetContentType()
@@ -125,6 +226,10 @@ class MsvcApi {
         return this.proxy
     }
 
+    /**
+     * Set request type as JSON.
+     * @returns {MsvcApi}
+     */
     json() {
         this.post()
         this.contentJson()
@@ -132,6 +237,10 @@ class MsvcApi {
         return this.proxy
     }
 
+    /**
+     * Send request.
+     * @returns {Promise<boolean|*>}
+     */
     async send() {
         await this.prepareSending()
 
@@ -151,12 +260,20 @@ class MsvcApi {
         }
     }
 
+    /**
+     * Apply preparing callbacks.
+     * @returns {Promise<void>}
+     */
     async prepareSending(){
         for(let preparingCallback of this.preparingCallbacks){
             await preparingCallback(this)
         }
     }
 
+    /**
+     * Handle microservice response.
+     * @returns {Promise<*>}
+     */
     async handleResponse() {
         this.handleServerErrors()
 
@@ -167,12 +284,18 @@ class MsvcApi {
         return this.decodedResponse
     }
 
+    /**
+     * Handle HTTP error of request.
+     */
     handleServerErrors() {
         if (this.response.ok === false) {
             throw new Error('Server error with code ' + this.response.status)
         }
     }
 
+    /**
+     * Handle errors of microservice response.
+     */
     handleResponseErrors() {
         if (this.decodedResponse['errors'] === undefined) return
 
@@ -183,6 +306,10 @@ class MsvcApi {
         }
     }
 
+    /**
+     * Create request target url.
+     * @returns {string}
+     */
     createRequestTarget() {
         let target = this.createRequestUrl()
 
@@ -194,12 +321,20 @@ class MsvcApi {
         return target
     }
 
+    /**
+     * Create request query from params.
+     * @returns {string}
+     */
     createDataQuery() {
         let params = new URLSearchParams(this.params)
 
         return params.toString()
     }
 
+    /**
+     * Create full request url.
+     * @returns {string}
+     */
     createRequestUrl() {
         let parts = [this.apiUrl, this.msvcName]
 
@@ -212,12 +347,19 @@ class MsvcApi {
         return parts.join('/')
     }
 
+    /**
+     * Change request type if exist file in request params.
+     */
     provideFileExistence() {
         if (this.dataToSendWithoutFiles()) return
 
         this.form()
     }
 
+    /**
+     * Check if request params contents File.
+     * @returns {boolean}
+     */
     dataToSendWithoutFiles() {
         if (typeof this.params !== 'object') return true
 
@@ -230,17 +372,26 @@ class MsvcApi {
         return true
     }
 
+    /**
+     * Set request content type for JSON.
+     * @returns {MsvcApi}
+     */
     contentJson() {
         this.fetchSettings.headers['Content-Type'] = this.jsonContentType
 
         return this.proxy
     }
 
+    /**
+     * Remove content type from fetch settings.
+     */
     resetContentType() {
         delete this.fetchSettings.headers['Content-Type']
     }
 
-
+    /**
+     * Provide request body.
+     */
     provideBody() {
         if (this.fetchSettings.method !== this.postMethod) return
 
@@ -253,10 +404,18 @@ class MsvcApi {
         }
     }
 
+    /**
+     * Create body for JSON request.
+     * @returns {string}
+     */
     createJsonBody() {
         return JSON.stringify(this.params)
     }
 
+    /**
+     * Create body for FORM request.
+     * @returns {FormData}
+     */
     createFormBody() {
         const formData = new FormData()
 
